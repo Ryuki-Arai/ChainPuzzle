93f5826b107ab5d6ed65a048594b9a8e2705eabe
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-11-15 15:10:44 +0900

    アウトライン追加(パフォーマンス改善版)

--- Assets/ChainPuzzle/Scripts/Piece/Piece.cs
+++ Assets/ChainPuzzle/Scripts/Piece/Piece.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityFx.Outline;
@@ -8,9 +9,16 @@ public class Piece : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private Renderer mesh;
-    [SerializeField] private OutlineBehaviour outline;
+    [SerializeField] private MeshRenderer outline;
+    [SerializeField] private MeshFilter outlineMesh;
     public PieceData PieceData { get; private set; }
 
+    private void Awake()
+    {
+        var Mesh = Instantiate(outlineMesh.sharedMesh);
+        Mesh.triangles = Mesh.triangles.Reverse().ToArray();
+        outlineMesh.sharedMesh = Mesh;
+    }
 
     public void Initialize(PieceData data)
     {
@@ -21,14 +29,16 @@ public class Piece : MonoBehaviour
     
     public void Select()
     {
-        outline.OutlineColor = Color.yellow;
+        //outline.material.color = Color.yellow;
+        outline.material.EnableKeyword("_EMISSION");
     }
 
     private void SetData()
     {
         text.text = PieceData.StrView;
         mesh.material = PieceData.Material;
-        outline.OutlineColor = Color.black;
+        //outline.material.color = Color.black;
+        outline.material.DisableKeyword("_EMISSION");
     }
 }
 
